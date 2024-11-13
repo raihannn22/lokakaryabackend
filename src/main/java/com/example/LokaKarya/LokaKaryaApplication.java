5ef8d920002f9b0178d2d325af83e471921411a4
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2024-11-13 15:17:11 +0700

    first commit

---
+++ src/main/java/com/example/LokaKarya/LokaKaryaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LokaKarya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokaKaryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokaKaryaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LokaKarya/LokaKaryaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LokaKarya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokaKaryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
