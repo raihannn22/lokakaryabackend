c61ee0ba5b7cc9963dd0e426a35db725842374c9
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2024-11-20 08:01:10 +0700

    all table ADD emp dev plan

---
+++ src/main/java/com/example/LokaKarya/Services/Impl/EmpDevPlanServImpl.java
@@ -0,0 +1,95 @@
+package com.example.LokaKarya.Services.Impl;
+
+import com.example.LokaKarya.Dto.EmpDevPlan.EmpDevPlanDto;
+import com.example.LokaKarya.Dto.EmpDevPlan.EmpDevPlanReqDto;
+import com.example.LokaKarya.Entity.DevPlan;
+import com.example.LokaKarya.Entity.EmpDevPlan;
+import com.example.LokaKarya.Entity.User;
+import com.example.LokaKarya.Repository.DevPlanRepo;
+import com.example.LokaKarya.Repository.EmpDevPlanRepo;
+import com.example.LokaKarya.Repository.UserRepo;
+import com.example.LokaKarya.Services.EmpDevPlanServ;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class EmpDevPlanServImpl implements EmpDevPlanServ {
+    private final Logger Log = LoggerFactory.getLogger(EmpDevPlanServImpl.class);
+
+    @Autowired
+    private EmpDevPlanRepo empDevPlanRepo;
+
+    @Autowired
+    private DevPlanRepo devPlanRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public List<EmpDevPlanReqDto> getAllEmpDevPlan() {
+        List<EmpDevPlan> response = empDevPlanRepo.findAll();
+        List<EmpDevPlanReqDto> empDevPlanReqDto = new ArrayList<>();
+        for (EmpDevPlan empDevPlan : response) {
+            empDevPlanReqDto.add(EmpDevPlanReqDto.fromEntity(empDevPlan));
+        }
+        return empDevPlanReqDto;
+    }
+
+    @Override
+    public EmpDevPlanReqDto getEmpDevPlanById(UUID id) {
+        EmpDevPlan empDevPlan = empDevPlanRepo.findById(id).orElseThrow(() -> new RuntimeException("EmpDevPlan not found"));
+        return EmpDevPlanReqDto.fromEntity(empDevPlan);
+    }
+
+    @Override
+    public EmpDevPlanReqDto createEmpDevPlan(EmpDevPlanDto empDevPlanDto) {
+        Log.info("Start createEmpDevPlan in EmpDevPlanServImpl");
+        EmpDevPlan empDevPlan = EmpDevPlanDto.toEntity(empDevPlanDto, UUID.randomUUID(), new Date(), null,null);
+        if (empDevPlanDto != null) {
+            Optional<DevPlan> devPlan = devPlanRepo.findById(empDevPlanDto.getDevPlan());
+            Optional<User> user = userRepo.findById(empDevPlanDto.getUser());
+            if (devPlan.isPresent() && user.isPresent()) {
+                empDevPlan.setDevPlan(devPlan.get());
+                empDevPlan.setUser(user.get());
+            } else {
+                throw new RuntimeException("User or DevPlan not found");
+            }
+        }
+        empDevPlanRepo.save(empDevPlan);
+        Log.info("End createEmpDevPlan in EmpDevPlanServImpl");
+        return EmpDevPlanReqDto.fromEntity(empDevPlan);
+    }
+
+    @Override
+    public EmpDevPlanReqDto updateEmpDevPlan(UUID id, EmpDevPlanDto empDevPlanDto) {
+        Log.info("Start updateEmpDevPlan in EmpDevPlanServImpl");
+        EmpDevPlan empDevPlan = empDevPlanRepo.findById(id).orElseThrow(() -> new RuntimeException("EmpDevPlan not found"));
+        if (empDevPlanDto != null) {
+            Optional<DevPlan> devPlan = devPlanRepo.findById(empDevPlanDto.getDevPlan());
+            Optional<User> user = userRepo.findById(empDevPlanDto.getUser());
+            if (devPlan.isPresent() && user.isPresent()) {
+                empDevPlan.setDevPlan(devPlan.get());
+                empDevPlan.setUser(user.get());
+            } else {
+                throw new RuntimeException("User or DevPlan not found");
+            }
+        }
+        empDevPlan.setId(id);
+        empDevPlan.setAssessmentYear(empDevPlanDto.getAssessmentYear());
+        empDevPlanRepo.save(empDevPlan);
+
+        Log.info("End updateEmpDevPlan in EmpDevPlanServImpl");
+        return EmpDevPlanReqDto.fromEntity(empDevPlan);
+    }
+
+    @Override
+    public Boolean deleteEmpDevPlan(UUID id) {
+        if (!empDevPlanRepo.existsById(id)) throw new RuntimeException("EmpDevPlan not found");
+        empDevPlanRepo.deleteById(id);
+        return true;
+    }
+}
