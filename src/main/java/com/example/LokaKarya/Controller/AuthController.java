c664a2383a2fe62d07b77a02ae62ab0362cb358d
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2024-11-21 11:01:19 +0700

    FIX USER LOGIIN RESPONSE

--- src/main/java/com/example/LokaKarya/Controller/AuthController.java
+++ src/main/java/com/example/LokaKarya/Controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.LokaKarya.Controller;
 
 import com.example.LokaKarya.Dto.LoginDto;
+import com.example.LokaKarya.Dto.LoginResponseDto;
+import com.example.LokaKarya.Dto.ManagerDto;
 import com.example.LokaKarya.Services.AuthServ;
 import com.example.LokaKarya.Services.UserServ;
 import org.slf4j.Logger;
@@ -22,7 +24,7 @@ public class AuthController {
     AuthServ authServ;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto data) {
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto data) {
         return ResponseEntity.ok(authServ.login(data));
     }
 }
