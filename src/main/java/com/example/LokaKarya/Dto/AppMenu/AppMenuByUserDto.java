a8375f059e57c633a8c50766dd17eaf84cbfc445
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2024-12-03 23:19:18 +0700

    add endpoint findmenu by user id

---
+++ src/main/java/com/example/LokaKarya/Dto/AppMenu/AppMenuByUserDto.java
@@ -0,0 +1,28 @@
+package com.example.LokaKarya.Dto.AppMenu;
+
+
+import com.example.LokaKarya.Entity.AppMenu;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@ToString
+public class AppMenuByUserDto {
+    @JsonProperty("ID")
+    private UUID id;
+
+    @JsonProperty("MENU_NAME")
+    private String menuName;
+
+    public static AppMenuByUserDto fromEntity(AppMenu appMenu) {
+        return AppMenuByUserDto.builder()
+                .id(appMenu.getId())
+                .menuName(appMenu.getMenuName())
+                .build();
+    }
+}
