19f68538f25a2a0b784a7b21ee723bb892fbd381
Ahmad Raihan <ahmad.reyhan2230@gmail.com>
2024-12-05 10:13:14 +0700

    add password otomatis generate

--- src/main/java/com/example/LokaKarya/Services/UserServ.java
+++ src/main/java/com/example/LokaKarya/Services/UserServ.java
@@ -6,11 +6,12 @@ import java.util.UUID;
 import com.example.LokaKarya.Dto.AppUserRole.AppUserRoleReqDto;
 import com.example.LokaKarya.Dto.User.UserDto;
 import com.example.LokaKarya.Dto.User.UserReqDto;
+import com.example.LokaKarya.Dto.User.UserReqUpdateDto;
 
 public interface UserServ {
     List<UserDto> getAllUsers();
     UserDto getUserById(UUID id);
     UserDto createUser(UserReqDto userDto);
-    UserDto updateUser(UUID id, UserReqDto userDto);
+    UserDto updateUser(UUID id, UserReqUpdateDto userDto);
     Boolean deleteUser(UUID id);
 }
